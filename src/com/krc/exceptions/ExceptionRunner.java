9794ca291d2322b866066d193fdd271c05d7f23e
Kurt Cooney <KCooney@interdev.com>
2023-10-19 09:29:58 -0400

    try catch

--- src/com/krc/exceptions/ExceptionRunner.java
+++ src/com/krc/exceptions/ExceptionRunner.java
@@ -3,8 +3,15 @@ package com.krc.exceptions;
 public class ExceptionRunner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String s = null;
+		getLength(s);
 	}
 
+	private static int getLength(String s) {
+		return metod2(s);
+	}
+
+	private static int metod2(String s) {
+		return s.length();
+	}
 }
